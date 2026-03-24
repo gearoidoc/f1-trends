3f3c96de5cd7393b37493b3c60328e5027136dd2
gearoidoc <itsgearofroad@gmail.com>
2026-03-24 20:27:09 +0000

    Initial Arrays for Driver Data

---
+++ drivers.js
@@ -0,0 +1,139 @@
+const CURRENT_DRIVERS = [
+    {
+        name: 'Max Verstappen',
+        team: 'Red Bull Racing',
+        keyword: 'Max Verstappen F1',
+        teamColor: '#3671C6'
+    },
+    {
+        name: 'Isack Hadjar',
+        team: 'Red Bull Racing',
+        keyword: 'Isack Hadjar F1',
+        teamColor: '#3671C6'
+    },
+    {
+        name: 'Charles Leclerc',
+        team: 'Ferrari',
+        keyword: 'Charles Leclerc F1',
+        teamColor: '#E8002D'
+    },
+    {
+        name: 'Lewis Hamilton',
+        team: 'Ferrari',
+        keyword: 'Lewis Hamilton F1',
+        teamColor: '#E8002D'
+    },
+    {
+        name: 'George Russell',
+        team: 'Mercedes',
+        keyword: 'George Russell F1',
+        teamColor: '#27F4D2'
+    },
+    {
+        name: 'Kimi Antonelli',
+        team: 'Mercedes',
+        keyword: 'Kimi Antonelli F1',
+        teamColor: '#27F4D2'
+    },
+    {
+        name: 'Lando Norris',
+        team: 'McLaren',
+        keyword: 'Lando Norris F1',
+        teamColor: '#FF8000'
+    },
+    {
+        name: 'Oscar Piastri',
+        team: 'McLaren',
+        keyword: 'Oscar Piastri F1',
+        teamColor: '#FF8000'
+    },
+    {
+        name: 'Fernando Alonso',
+        team: 'Aston Martin',
+        keyword: 'Fernando Alonso F1',
+        teamColor: '#229971'
+    },
+    {
+        name: 'Lance Stroll',
+        team: 'Aston Martin',
+        keyword: 'Lance Stroll F1',
+        teamColor: '#229971'
+    },
+    {
+        name: 'Pierre Gasly',
+        team: 'Alpine',
+        keyword: 'Pierre Gasly F1',
+        teamColor: '#FF87BC'
+    },
+    {
+        name: 'Franco Colapinto',
+        team: 'Alpine',
+        keyword: 'Franco Colapinto F1',
+        teamColor: '#FF87BC'
+    },
+    {
+        name: 'Alexander Albon',
+        team: 'Williams Racing',
+        keyword: 'Alexander Albon F1',
+        teamColor: '#64C4FF'
+    },
+    {
+        name: 'Carlos Sainz',
+        team: 'Williams Racing',
+        keyword: 'Carlos Sainz F1',
+        teamColor: '#64C4FF'
+    },
+    {
+        name: 'Esteban Ocon',
+        team: 'Haas F1 Team',
+        keyword: 'Esteban Ocon F1',
+        teamColor: '#B6BABD'
+    },
+    {
+        name: 'Oliver Bearman',
+        team: 'Haas F1 Team',
+        keyword: 'Oliver Bearman F1',
+        teamColor: '#B6BABD'
+    },
+    {
+        name: 'Liam Lawson',
+        team: 'Racing Bulls',
+        keyword: 'Liam Lawson F1',
+        teamColor: '#6692FF'
+    },
+    {
+        name: 'Arvid Lindblad',
+        team: 'Racing Bulls',
+        keyword: 'Arvid Lindblad F1',
+        teamColor: '#6692FF'
+    },
+    {
+        name: 'Nico Hulkenberg',
+        team: 'Audi',
+        keyword: 'Nico Hulkenberg F1',
+        teamColor: '#BB0A14'
+    },
+    {
+        name: 'Gabriel Bortoleto',
+        team: 'Audi',
+        keyword: 'Gabriel Bortoleto F1',
+        teamColor: '#BB0A14'
+    },
+    {
+        name: 'Valterri Bottas',
+        team: 'Cadillac',
+        keyword: 'Valterri Bottas F1',
+        teamColor: '#C8A96A'
+    },
+    {
+        name: 'Sergio Perez',
+        team: 'Cadillac',
+        keyword: 'Sergio Perez F1',
+        teamColor: '#C8A96A'
+    }
+]
+
+// For historical drivers, Future addition
+const HISTORICAL_DRIVERS = []
+
+export { CURRENT_DRIVERS, HISTORICAL_DRIVERS }
